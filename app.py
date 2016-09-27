f25a062b82e67af0ccd73e3713d5b948caf7758f
Kathy Lau <imobilep501@gmail.com>
2016-09-27 14:04:40 -0400

    done

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    d = utils.occu.create_dictionary('occupations.csv')
+    d = utils.occu.create_dictionary('data/occupations.csv')
     return render_template("template.html", foo="Occupations", occuDict=d, randjob = utils.occu.chooser(d))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
